7df4b29170d83a8cb66144346a7ea59f2a9dbd91
Sanderson Zamorano <sanderson.zamorano@beedelivery.com.br>
2024-07-12 20:03:05 -0300

    feat: add navbar and footer components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <script setup lang='ts'>
-import Button from './components/ui/button/Button.vue'
+import Navbar from './components/personal/navbar/Navbar.vue';
+import Footer from './components/personal/footer/Footer.vue';
 
-const test = () => {
-  document.body.classList.toggle('red')
-}
 </script>
 
 <template>
-  <div class="h-screen dark:bg-gray-900">
-    <Button @click="test()"> 
-      Toggle Dark Mode
-    </Button>
+  <div class="h-screen flex flex-col justify-between dark:bg-gray-900">
+    <Navbar />
+
+    <div class="flex flex-1 flex-col justify-center items-center bg-gray-900">
+      <div class="">
+          <div class="flex flex-col items-center space-y-4 bg-gray-800 p-6 rounded">
+            <div class="flex space-x-4">
+              <button class="px-4 py-2 bg-blue-500 text-white rounded">Focus</button>
+              <button class="px-4 py-2 bg-green-500 text-white rounded">Break</button>
+            </div>
+            <div class="text-9xl font-bold text-white">00:00:00</div>
+            <button class="px-4 py-2 bg-red-500 text-white rounded">Start</button>
+          </div>
+      </div>
+    </div>
+
+    <Footer />
   </div>
 </template>
---
+++ src/components/personal/footer/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <footer class="bg-gray-900 p-6 text-white flex">
+        <div class="flex flex-1 size-4 justify-center items-center">Made with ❤ by Sanderson
+            Zamorano</div>
+    </footer>
+</template>
+
+
+<style scoped>
+* {
+    box-sizing: border-box;
+    font-family: 'Product Sans', 'Roboto', sans-serif;
+}
+</style>
