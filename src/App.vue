2fda6b54cfe9accc6be4d24909f314d566758ae0
Sanderson Zamorano <sanderson.zamorano@beedelivery.com.br>
2024-07-13 17:13:35 -0300

    feat: create timer component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <script setup lang='ts'>
 import Navbar from './components/personal/navbar/Navbar.vue';
 import Footer from './components/personal/footer/Footer.vue';
-
+import Timer from './components/personal/timer/Timer.vue';
 </script>
 
 <template>
   <div class="h-screen flex flex-col justify-between dark:bg-gray-900">
     <Navbar />
 
-    <div class="flex flex-1 flex-col justify-center items-center bg-gray-900">
-      <div class="">
-          <div class="flex flex-col items-center space-y-4 bg-gray-800 p-6 rounded">
-            <div class="flex space-x-4">
-              <button class="px-4 py-2 bg-blue-500 text-white rounded">Focus</button>
-              <button class="px-4 py-2 bg-green-500 text-white rounded">Break</button>
-            </div>
-            <div class="text-9xl font-bold text-white">00:00:00</div>
-            <button class="px-4 py-2 bg-red-500 text-white rounded">Start</button>
-          </div>
+    <div class="flex flex-1 bg-gray-900">
+      <div class="flex flex-1 flex-col justify-center items-center">
+        <p class="text-white text-start">asjdfasnfk</p>
+        <Timer />
       </div>
     </div>
 
---
+++ src/components/personal/timer/Timer.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex flex-col items-center space-y-4 bg-gray-800 p-6 rounded">
+        <div class="flex flex-1 justify-around gap-2 w-full">
+            <button class="w-full px-4 py-2 bg-gray-700 text-white rounded">Focus</button>
+            <button class="w-full px-4 py-2 bg-gray-600 text-white rounded">Break</button>
+        </div>
+        <div class="text-9xl font-bold text-white">00:00</div>
+        <button class="px-4 py-2 w-2/3 bg-white rounded">Start</button>
+    </div>
+</template>
